84755c2554feabd4835ef7e5ead42f12a287075c
Dikshant Choudhary <dikshantchoudhary12345@gmail.com>
2025-08-25 23:53:06 +0530

    continue

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -1,8 +1,41 @@
 <script>
+  import ProductCard from "./ProductCard.svelte";
   import SectionWrapper from "./SectionWrapper.svelte";
   let productFeatures = [
-
-  ]
+        {
+            featureList: [
+                "Daily streak counter",
+                "Weights and reps tracker",
+                "Community challenges",
+                "Every workout is new and exciting",
+            ],
+            description:
+                "Workout Zone ensures all users have the information they need to train safely and train effectively.",
+            imgUrl: "assets/selection.png",
+        },
+        {
+            featureList: [
+                "Select your preferences",
+                "Personalize your workouts",
+                "Customize your training",
+                "Guarantees progress on all core exercises",
+            ],
+            description:
+                "Build you streaks and see your progress with our amazing tracking and analytics features.",
+            imgUrl: "assets/training.png",
+        },
+        {
+            featureList: [
+                "Eliminating guesswork",
+                "Save time",
+                "Achieve the performance you seek",
+                "Builds functional strength",
+            ],
+            description:
+                "Enter as much information about yourself as you desire to ensure all workouts are personalized to your every need.",
+            imgUrl: "assets/body.jpg",
+        },
+    ];
 </script>
 
 <SectionWrapper id="product">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
   import Hero from "../components/Hero.svelte";
   import Product from "../components/Product.svelte";
+  import Reviews from "../components/Reviews.svelte";
 
 </script>
 <main class="flex flex-col flex-1">
   <Hero />
   <Product />
-
+  <Reviews />
 </main>
